478f2d90eea74dd8fe7ab29c93f04549b9cd1f91
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-06 17:47:08 +0545

    choosing the right value of alpha for your model

---
+++ feature_scaling/learn_rate.py
@@ -0,0 +1,11 @@
+#we can also choose a good learning rate to find out the cost function more properly 
+
+#if the cost function plot is periodic, then the learning rate is very large 
+
+#large learning rate can sometimes mean offshooting from the required value 
+
+# values of alpha to try 0.001 0.01 0.1, 1
+
+# with a difference of 3 
+
+#find the largest possible alpha that doesnot create offshooting 
